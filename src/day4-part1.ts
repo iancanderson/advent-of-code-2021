fbdee09f3b0e7c3afa91d3a74a51ecd984656277
Ian C. Anderson <ian@iancanderson.com>
2021-12-08 08:13:18 -0500

    Day 4

---
+++ src/day4-part1.ts
@@ -0,0 +1,114 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync("src/day4-input.txt", "utf8");
+const lines = input.split("\n");
+const numbersCalled = lines[0].split(",").map(Number);
+
+interface CalledBingoNumber {
+  called: true;
+}
+interface UncalledBingoNumber {
+  called: false;
+  number: number;
+}
+type BingoNumber = CalledBingoNumber | UncalledBingoNumber;
+
+class BingoCard {
+  #rows: BingoNumber[][];
+  #columns: BingoNumber[][] = [];
+
+  constructor(numbersByRow: number[][]) {
+    this.#rows = numbersByRow.map((row) =>
+      row.map((number) => ({ called: false, number }))
+    );
+    for (let col = 0; col < numbersByRow.length; col++) {
+      this.#columns.push([]);
+
+      for (let row = 0; row < numbersByRow.length; row++) {
+        this.#columns[col].push(this.#rows[row][col]);
+      }
+    }
+  }
+
+  callNumber(num: number) {
+    this.#rows = this.#markNumbersAsCalled(this.#rows, num);
+    this.#columns = this.#markNumbersAsCalled(this.#columns, num);
+  }
+
+  hasWon(): boolean {
+    return (
+      this.#rows.some((row) => row.every((number) => number.called)) ||
+      this.#columns.some((col) => col.every((number) => number.called))
+    );
+  }
+
+  score(winningNumber: number): number {
+    const uncalledNumberSum = this.#rows.reduce((sum, row) => {
+      return (
+        sum +
+        row.reduce((sum, number) => {
+          return number.called ? sum : sum + number.number;
+        }, 0)
+      );
+    }, 0);
+    return uncalledNumberSum * winningNumber;
+  }
+
+  display(): string {
+    return this.#rows
+      .map((row) =>
+        row.map((number) => (number.called ? "X" : number.number)).join(" ")
+      )
+      .join("\n");
+  }
+
+  #markNumbersAsCalled(nums: BingoNumber[][], num: number): BingoNumber[][] {
+    return nums.map((row) => {
+      return row.map((number) => {
+        if (!number.called && number.number === num) {
+          return { called: true };
+        } else {
+          return number;
+        }
+      });
+    });
+  }
+}
+
+function buildBingoCards(): BingoCard[] {
+  const bingoCards = [];
+  for (let l = 2; l < lines.length; l += 6) {
+    let numbers: number[][] = [];
+    for (let row = 0; row < 5; row++) {
+      numbers.push(
+        lines[l + row]
+          .trim()
+          .split(/\s+/)
+          .map((str) => parseInt(str))
+      );
+    }
+    const card = new BingoCard(numbers);
+    console.log(card.display());
+    bingoCards.push(card);
+  }
+  return bingoCards;
+}
+
+const bingoCards = buildBingoCards();
+
+for (const num of numbersCalled) {
+  console.log("Calling number: ", num);
+
+  for (const card of bingoCards) {
+    card.callNumber(num);
+    // console.log(card.display());
+  }
+  const winningCard = bingoCards.filter((card) => card.hasWon())[0];
+
+  if (winningCard) {
+    console.log("Winning card:\n", winningCard.display());
+    console.log("Winning number: ", num);
+    console.log("Winning card score: ", winningCard.score(num));
+    break;
+  }
+}
